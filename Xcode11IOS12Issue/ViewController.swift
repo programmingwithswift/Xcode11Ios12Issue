b52bf3ad6c5cb38f3e798a37f2ab40c46a759855
MrBlue <dleak22@gmail.com>
2020-05-02 14:57:02 +0200

    Initial commit

---
+++ Xcode11IOS12Issue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode11IOS12Issue
+//
+//  Created by ProgrammingWithSwift on 2020/05/02.
+//  Copyright © 2020 ProgrammingWithSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
